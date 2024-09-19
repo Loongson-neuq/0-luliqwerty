e2862f5dec20c1339f4615926955b444f57b94fb
HDQ <3535768174@qq.com>
2024-09-19 23:37:42 +0800

    complete assignment

--- main.cpp
+++ main.cpp
@@ -4,4 +4,22 @@
 void CompareThreeNumbers(int a, int b, int c, Result &result)
 {
     // 在这里完成你的代码
+    if (a > b) {
+        int t = a;
+        a = b;
+        b = t;
+    }
+    if (b > c) {
+        int t = b;
+        b = c;
+        c = t;
+    }
+    if (a > c) {
+        int t = a;
+        a = c;
+        c = t;
+    }
+    result.min = a;
+    result.mid = b;
+    result.max = c;
 }
